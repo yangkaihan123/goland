0f088436096ce41f4dd42ddf9ebccba871c3c14a
bryant <bryant.ba520@gmail.com>
2022-06-10 16:47:38 +0800

    update leetcode day4

---
+++ leetcode/romanToint.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "IDI"
+	c := romanToInt(s)
+	fmt.Println("III = ", c)
+}
+
+func romanToInt(s string) int {
+	m := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	n := len(s)
+	ans := m[s[n-1]]
+	for i := n - 2; i >= 0; i-- {
+		if m[s[i]] >= m[s[i+1]] {
+			ans += m[s[i]]
+		} else {
+			ans -= m[s[i]]
+		}
+	}
+	return ans
+}
