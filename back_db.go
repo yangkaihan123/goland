3b41ee6fd391940be5844c74144e0ac1f2d770d8
bryant <bryant.ba520@gmail.com>
2022-06-24 16:19:16 +0800

    update some shit

--- back_db.go
+++ back_db.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/spf13/viper"
 	"io/ioutil"
 	"log"
 	"os/exec"
@@ -8,31 +10,51 @@ import (
 )
 
 func main() {
-	BackupMySqlDb("localhost", "3306", "root", "1234qwer", "school", "", "E:/")
+	viper.SetConfigName("mysql-config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Println(err)
+	}
+
+	host := viper.GetString("mysql.host")
+	port := viper.GetString("mysql.port")
+	user := viper.GetString("mysql.user")
+	password := viper.GetString("mysql.password")
+	databaseName := viper.GetString("mysql.databaseName")
+	tableName := viper.GetString("mysql.tableName")
+	sqlPath := viper.GetString("mysql.sqlPath")
+	fmt.Println(host, port, user, password, databaseName, tableName, sqlPath)
+	BackupMySqlDb(host, port, user, password, databaseName, tableName, sqlPath)
 }
 
 /**
- *
- * 备份MySql数据库
- * @param   host:           数据库地址: localhost
- * @param   port:           端口: 3306
- * @param   user:           用户名: root
- * @param   password:       密码: root
- * @param   databaseName:   需要被分的数据库名: test
- * @param   tableName:      需要备份的表名: user
- * @param   sqlPath:        备份SQL存储路径: D:/backup/test/
- * @return  backupPath
- *
+*
+* 备份MySql数据库
+* @param 	host: 			数据库地址: localhost
+* @param 	port:			端口: 3306
+* @param 	user:			用户名: root
+* @param 	password:		密码: root
+* @param 	databaseName:	需要被分的数据库名: test
+* @param 	tableName:		需要备份的表名: user
+* @param 	sqlPath:		备份SQL存储路径: D:/backup/test/
+* @return 	backupPath
+*
  */
+
 func BackupMySqlDb(host, port, user, password, databaseName, tableName, sqlPath string) (error, string) {
 	//定义Cmd结构体对象指针
 	var cmd *exec.Cmd
 
 	//在这里如果没有传输表名，那么将会备份整个数据库，否则将只备份自己传入的表
+
 	if tableName == "" {
-		cmd = exec.Command("mysqldump", "--opt", "-h"+host, "-P"+port, "-u"+user, "-p"+password, databaseName)
+		cmd = exec.Command("mysqldump", "--opt", "-h "+host, "-P "+port, "-u "+user, "-p "+password, databaseName)
+		fmt.Println(cmd)
 	} else {
-		cmd = exec.Command("mysqldump", "--opt", "-h"+host, "-P"+port, "-u"+user, "-p"+password, databaseName, tableName)
+		cmd = exec.Command("mysqldump", "--opt", "-h "+host, "-P "+port, "-u "+user, "-p "+password, databaseName, "-t "+tableName)
+		fmt.Println(cmd)
 	}
 
 	//StdinPipe方法返回一个在命令Start后与命令标准输入关联的管道。
@@ -53,14 +75,14 @@ func BackupMySqlDb(host, port, user, password, databaseName, tableName, sqlPath 
 		return err, ""
 	}
 	//获得一个当前的时间戳
-	now := time.Now().Format("20060102150405")
+	now := time.Now().Format("20220623")
 	var backupPath string
 
 	//设置我们备份文件的名字
 	if tableName == "" {
-		backupPath = sqlPath + databaseName + "_" + now + ".sql"
+		backupPath = sqlPath + databaseName + now + ".sql"
 	} else {
-		backupPath = sqlPath + databaseName + "_" + tableName + "_" + now + ".sql"
+		backupPath = sqlPath + databaseName + tableName + "_" + now + ".sql"
 	}
 	//写入文件并设置文件权限
 	err = ioutil.WriteFile(backupPath, bytes, 0644)
